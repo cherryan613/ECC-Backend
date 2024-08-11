b7a7813bd9d286c8fa2ecfc0d0e42c90e7017507
cherryan613 <cherry613@ewhain.net>
2024-08-11 22:14:12 +0900

    chore/product: ProductCode로 리스트가 아닌 객체를 반환하도록 수정

--- src/main/java/com/example/backend/repository/ProductRepository.java
+++ src/main/java/com/example/backend/repository/ProductRepository.java
@@ -3,16 +3,15 @@ package com.example.backend.repository;
 import com.example.backend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long>, CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query(value = "SELECT * FROM products WHERE product_code = :product_code", nativeQuery = true)
-    List<Product> findByProductCode(Long product_code);
+    Product findByProductCode(Long product_code);
 
     @Query(value = "SELECT * FROM products WHERE product_type = :product_type", nativeQuery = true)
     List<Product> findByProductType(String product_type);
